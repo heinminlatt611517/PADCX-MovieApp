ec51d9fcb544fe6a9067e8cf48c61bccde39afeb
nawzarchilinn@Mocs-MacBook-Air-2.local <norzarchi@myanmaronlinecreations.com>
2020-08-01 22:53:47 +0630

    set up Snack Bar

--- app/src/main/java/com/example/padcx_movieapp_assignment/activities/BaseActivity.kt
+++ app/src/main/java/com/example/padcx_movieapp_assignment/activities/BaseActivity.kt
@@ -2,10 +2,15 @@ package com.example.padcx_movieapp_assignment.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun showSnackBar (message: String) {
+        Snackbar.make(window.decorView, message, Snackbar.LENGTH_SHORT).show()
     }
 }
